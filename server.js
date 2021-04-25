d09ad9efd99268315d134c00badf55d39cb3a5cd
JGreenOS <jsciteach@gmail.com>
2021-04-24 21:33:44 -0400

    fixing server connection

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 const app = express();
