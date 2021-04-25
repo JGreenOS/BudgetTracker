2506196ac82ad1dcc7a110fdaff872676cccce84
JGreenOS <jsciteach@gmail.com>
2021-04-25 16:33:43 -0400

    connection configs

--- server.js
+++ server.js
@@ -10,16 +10,25 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-
+// console.log("process env", process.env.MONGODB_URI);
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/budgettracker',
+    process.env.MONGODB_URI,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true,
         useFindAndModify: false
 });
+
+mongoose.connection.on('connected', () =>
+  console.log('Connected to MongoDB Endpoint')
+);
+
+mongoose.connection.on('error', (err) =>
+  console.log(`Mongoose default connection error: ${err}`)
+);
+
 
 //get the routes from here
 app.use(require("./routes/api.js"));
